497e681b15aca8d7e593fd34d19f283969e9fa8d
Erős Ákos <wlxgpb@neptun.gdf.hu>
2022-05-22 13:10:33 +0200

    initial commit

---
+++ src/hu/gdf/ElosztottAlkalmazasok.java
@@ -0,0 +1,13 @@
+package hu.gdf;
+
+/**
+ * Elosztott alkalmazások beadandó feladat
+ * @author Erős Ákos <wlxgpb@neptun.gdf.hu>
+ */
+public class ElosztottAlkalmazasok {
+
+    public static void main(String[] args) {
+        System.out.println("Futok!");
+    }
+    
+}
